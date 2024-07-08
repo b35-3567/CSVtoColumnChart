ab9185dc97e54c9307f02b2168df06bc90efb8bb
thanh danh <danhlt1510@gmail.com>
2024-07-08 11:58:07 +0700

    first commit

---
+++ num_test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Đọc dữ liệu từ các tệp CSV
+fpt_data = pd.read_csv('FPT.csv')
+pnj_data = pd.read_csv('PNJ.csv')
+msn_data = pd.read_csv('MSN.csv')
+vic_data = pd.read_csv('VIC.csv')
